a3df4c6b87b26f8527f034817564b6f2e2308078
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-09 15:13:51 -0700

    Expression worksheet
    Dog Years

--- Expressions_worksheet/JS/script.js
+++ Expressions_worksheet/JS/script.js
@@ -1,2 +1,13 @@
 // Karri Wright Oct. 9, 2014 Expression Worksheet
 
+//Calculate Sparky's age in human years
+
+var dogAge = 7;
+var humAge = 5;
+
+//multiply the dog age by human age.
+
+var actAge = dogAge * humAge;
+//print out the total of how old Sparky is in dog years
+console.log("Sparky's age in dog years is" + " " + actAge);
+
