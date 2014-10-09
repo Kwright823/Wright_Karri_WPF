2d3df02e3039cbdc83fb171617854997c8f7bab7
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-09 15:37:24 -0700

    expression worksheet
    pizza part 2

--- Expressions_worksheet/JS/script.js
+++ Expressions_worksheet/JS/script.js
@@ -5,23 +5,32 @@
 var dogAge = 7;
 var humAge = 5;
 
-//multiply the dog age by human age.
+//multiply the dog age 7 by human age 5.
 
 var actAge = dogAge * humAge;
-//print out the total of how old Sparky is in dog years
+//print out the total of how old Sparky is in dog years.
 console.log("Sparky is" + " " + humAge + " " + "human years old which is" + " " + actAge + " " + "in dog years");
 
 
 
 //How many slices of pizza will each partygoer have
 
-var slicePerPizza = 8;
+var slicePerPizza = 9;
 var partyGoers = 6;
 var pizzasOrdered = 3;
 
-//total how many slices there are in all by multiplying 8 and 3 then dividing by amount of partygoers 6
+//total how many slices there are in all by multiplying 9 and 3 then dividing by amount of partygoers 6
 
 var slicePerPerson = slicePerPizza * pizzasOrdered / partyGoers;
 console.log("Each person ate" + " " + slicePerPerson + " " + "slices of pizza at the party.");
+
+
+//How many slices will Sparky get?
+
+var partyPeople = 10
+var pizzas = 4
+var slices = 8
+var sparky = s
+
 
 
