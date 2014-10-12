6d9d77d2d8eedd0dea9dd14d2a3500e651a6effd
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-12 16:46:35 -0700

    Expressions
    Personal

--- ExpressionsPersonal/js/script.js
+++ ExpressionsPersonal/js/script.js
@@ -2,12 +2,14 @@
 
 //How much money can I save before my friend's wedding next year?
 
-var saveTotal = prompt("How much total would you like to have for spending money?");
+var saveTotal = prompt("How much do you have available to save per month?");
 var timeFrame = prompt("How many months until the wedding?");
-var totalPerMonth = timeFrame / saveTotal;
+var totalPerMonth = timeFrame * saveTotal;
 console.log(totalPerMonth); //print it out to console
 
 var result = "The total you need to save per month is $" + totalPerMonth;
 alert(result);
+
+
 
 
