eaade66c9ebeee1fd47b14821991898bfce0ff94
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-12 18:15:03 -0700

    Expression
    Wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,11 +3,11 @@
 //How much wood could a woodchuck chuck if a woodchuck could chuck wood?
 
 var wood = prompt("How many pieces of wood do you have?");
-var woodChuck = prompt("How many woodchucks are there with you?");
-var couldChuck = prompt("Do you think a woodchuck could chuck wood?");
+var woodChuck = prompt("How many Woodchucks are there with you?");
+var couldChuck = prompt("Do you think a Woodchuck could chuck wood?");
 console.log(wood);
 console.log(woodChuck);
 console.log(couldChuck);
 
 var totalWood = wood * woodChuck;
-alert(totalWood + " " + "is how much wood a woodchuck could chuck if a Woodchuck could chuck wood.");
+alert(totalWood + " " + "pieces of wood is how much wood a Woodchuck could chuck if a Woodchuck could chuck wood.");
