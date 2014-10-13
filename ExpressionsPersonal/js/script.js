92fdfc1ac5e14feb41172fd5763ab9313476a19b
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-12 17:45:46 -0700

    expressions
    personal

--- ExpressionsPersonal/js/script.js
+++ ExpressionsPersonal/js/script.js
@@ -2,14 +2,23 @@
 
 //How much money can I save before my friend's wedding next year?
 
-var saveTotal2 = prompt("How much do you have available per week?");
-var saveTotal = prompt("How much do you have available to save per month?");
-var timeFrame = prompt("How many months until the wedding?");
-var totalPerMonth = timeFrame * saveTotal;
-console.log(totalPerMonth); //print it out to console
+var saveTotal2 = prompt("How much money do you have available to save per week?");
+var saveTotal = prompt("How much money do you have available to save per month?");
+var timeFrame1 = prompt("How many weeks until the wedding?");
+var timeFrame2 = prompt("How many months until the wedding?");
+
+
+var totalWeek = saveTotal * timeFrame1;
+var totalMonth = saveTotal2 * timeFrame2;
+//console.log(totalPerMonth); //print it out to console
 
 //var result = "The total you need to save per month is $" + totalPerMonth;
 //alert(result);
+
+alert ("If you save" + " " + "$" + saveTotal2 + " " + "a week, you will have" +
+" " + totalWeek + " " + "available to spend." +
+" " + "Or if you save $" + " " + saveTotal + " " + "a month, you will have $" +
+" " + totalMonth + " " + "available to spend.");
 
 
 
