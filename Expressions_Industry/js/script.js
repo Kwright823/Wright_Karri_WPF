8d99293355c9af0e5afb11e92f957decac544703
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-12 18:03:12 -0700

    Expression
    Industry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,8 +5,14 @@
 
 var checks = [123, 456, 789]; //array of checks
 var cash = [91, 82, 73]; //array of cash
+var cashBack = 10;
 
 var totalDep = checks[0] + checks[1] + checks[2] +
-    cash[0] + cash[1] + cash[2]; //totaling up all the arrays
+    cash[0] + cash[1] + cash[2] - cashBack; //totaling up all the arrays
 console.log(totalDep);
+
+alert(totalDep);
+
+//I am not sure how to do pop ups with an array
+//Is there a way to categorize the popups with a certain index # to put them into an array?
 
