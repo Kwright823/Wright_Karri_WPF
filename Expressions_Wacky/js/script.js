522630c665fa72ac410386912697de31e2d5ce59
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-12 17:27:23 -0700

    Expression
    Wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,4 +2,5 @@
 
 //How many bobby pins do my daughter and I go through in 10 years?
 
-
+var bobbyPin1 = prompt("How many bobby pins does Karri use in a week?");
+var bobbyPin2 = prompt("How many bobby pins does Ava use in a week?")
