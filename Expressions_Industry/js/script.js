f12606e25b11d506b07a860581ad042dedcc7194
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-12 17:22:14 -0700

    Expressions
    Industry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,4 +3,10 @@
 
 //I am a stay at home mom at the moment, however, I did recently work at KeyBank as a teller
 
-var checks = prompt("Enter in the amount of your check")
+var checks = [123, 456, 789]; //array of checks
+var cash = [91, 82, 73]; //array of cash
+
+var totalDep = checks[0] + checks[1] + checks[2] +
+    cash[0] + cash[1] + cash[2]; //totaling up all the arrays
+console.log(totalDep);
+
