b385e508c346103759f7338771e81f4b72a158f6
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-12 18:06:17 -0700

    Expressions
    wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,8 @@
 //Karri Wright October 9, 2014 Expressions_Wacky
 
-//
+//How much wood could a wood chuck chuck if a wood chuck could chuck wood?
+
+var wood = prompt("How much wood do you have?");
+var woodChuck = prompt("How many wood chucks are there with you?");
+var distance = prompt("What is the distance we are looking at?");
+
