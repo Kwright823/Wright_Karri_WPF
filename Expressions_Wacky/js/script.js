35725627c01015087a2728ed8a92958d76bddb5d
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-12 17:27:58 -0700

    Expression
    Wacky changed idea

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,6 +1,2 @@
 //Karri Wright October 9, 2014 Expressions_Wacky
 
-//How many bobby pins do my daughter and I go through in 10 years?
-
-var bobbyPin1 = prompt("How many bobby pins does Karri use in a week?");
-var bobbyPin2 = prompt("How many bobby pins does Ava use in a week?")
