aad6f5972946156e46cb2eb96cebb668b7cc977f
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-16 16:34:18 -0700

    Conditionals
    Personal

--- Conditional_Personal/js/script.js
+++ Conditional_Personal/js/script.js
@@ -1,13 +1,17 @@
 //Karri Wright Conditionals Personal
 
 var weather = prompt("Is the weather warm or cold?");
-var warm;
-var cold;
+var warm = wearATanktop;
+var cold = wearASweater;
 
 if(weather === warm){
        wearATanktop();
 }
+
+
 if(weather === cold){
         wearASweater();
 }
 console.log(weather);
+
+var result = ("")
