7b91f735496e4f7923c3c9f24ea30f84294e818c
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-20 14:07:34 -0700

    conditional
    personal

--- Conditional_Personal/js/script.js
+++ Conditional_Personal/js/script.js
@@ -1,17 +1,19 @@
 //Karri Wright Conditionals Personal
 
 var weather = ["warm", "cold"](prompt("Is the weather warm or cold today?"));
-if weather = " "
+if (weather = " "){//if no answer prompt again
+    alert("Please answer: Is the weather warm or cold?");//ask again
+}
+console.log(weather);
+
+if (weather === "warm") {//if warm wear a tank top
+    alert("Wear a tanktop!");//alert
+    console.log("You should wear a tank top today!");//log info
 
 
-if (weather === "warm") {
-    alert("Wear a tanktop!";
-    console.log("You should wear a tank top today!");
-
-
-}else if(weather === "cold"){
+}else if(weather === "cold"){//if cold wear a hoodie
     alert("Wear a hoodie!");
-    console.log("You should wear a sweater today!");
+    console.log("You should wear a hoodie today!");
 
 
 }
