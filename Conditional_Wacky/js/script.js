475e6bf7bfa1b8e501d75d689369653065e969fe
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-20 14:50:46 -0700

    conditionals
    wacky

--- Conditional_Wacky/js/script.js
+++ Conditional_Wacky/js/script.js
@@ -2,24 +2,20 @@
 
 //What movie would you like to watch today?
 
-var movie = prompt("Let us figure what movie you would like to watch. \n " +
+var movie = prompt("Let us suggest a movie for you to watch. \n" +
     "What are you in the mood for today? \n" +
     "Action, romcom, bromance, or musical?");
 
 if(movie = "action"){
     console.log("You should watch Gladiator");
     alert("You should watch Gladiator")
-}
-if(movie = "romcom"){
+}else if(movie = "romcom"){
     console.log("You should watch What to Expect When Expecting");
     alert("You should watch What to Expect When Expecting")
-}
-if(movie = "bromance"){
+}else if(movie = "bromance"){
     console.log("You should watch This is the End");
     alert("You should watch This is the End")
-}
-if(movie = "musical"){
+}else if(movie = "musical"){
     console.log("You should watch Labyrinth");
     alert("You should watch Labyrinth")
-
-} 
+}
