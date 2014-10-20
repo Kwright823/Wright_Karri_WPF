70ff61b8b0b9c2d9c110519651cfd9beb469af1a
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-20 14:48:37 -0700

    conditionals
    wacky

--- Conditional_Wacky/js/script.js
+++ Conditional_Wacky/js/script.js
@@ -14,3 +14,12 @@ if(movie = "romcom"){
     console.log("You should watch What to Expect When Expecting");
     alert("You should watch What to Expect When Expecting")
 }
+if(movie = "bromance"){
+    console.log("You should watch This is the End");
+    alert("You should watch This is the End")
+}
+if(movie = "musical"){
+    console.log("You should watch Labyrinth");
+    alert("You should watch Labyrinth")
+
+} 
