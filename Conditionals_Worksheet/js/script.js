3f280ecd392d02542c48f653590c1e4d48cdc4a3
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-20 16:07:51 -0700

    Conditionals
    Worksheet
    C to F

---
+++ Conditionals_Worksheet/js/script.js
@@ -0,0 +1,10 @@
+//Conditionals_Worksheet Karri Wright
+
+//Celsius to Fahrenheit converter
+
+var degFahren = Number(prompt("Enter the degrees Fahrenheit", 32));
+var degCent;
+
+degCent = 5/9 * (degFahren - 32);
+console.log(degFahren + "Fahrenheit is " + degCent + " centigrade.");
+
