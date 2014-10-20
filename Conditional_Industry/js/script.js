7b911fd4543f2673aeec1cefe45eab1c7a82a1e4
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-20 14:38:34 -0700

    conditionals
    industry

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -1,13 +1,13 @@
 //Karri Wright Conditionals Industry
 
 //will the sign fit on the sign board?
-
+//display board measurements
 var width = prompt("Lets find out if the sign will fit on the display board n/ What is the width of the display board?");
 var length = prompt("What is the length of the display board?");
-
+//sign measurements
 var signWidth = prompt("What is the width of the sign?");
 var signLength = prompt("What is the length of the sign?");
-
+//area of each
 var boardArea = width * length;
 
 var signArea = signWidth * signLength;
