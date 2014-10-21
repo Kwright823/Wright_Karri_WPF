8ffb4196b27f155155e1d234223b1ff593557206
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-20 22:49:56 -0700

    Conditionals WS
    worksheet

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -1,7 +1,7 @@
 //Conditionals_Worksheet Karri Wright
 
 //Celsius to Fahrenheit converter
-var unit = prompt("Please enter the unit of degrees.");
+/*var unit = prompt("Please enter the unit of degrees.");
 console.log(unit);
 
 var temp = prompt("Are we converting to Celsius or to Fahrenheit?" +
@@ -18,3 +18,25 @@ if(temp === "C"){
     console.log("The temperature is " + fahr + " degrees Fahrenheit.");
     alert("The temperature is " + fahr + " degrees Fahrenheit.")
 }
+/*
+
+ */
+//Last Chance for Gas!
+//MPG, gauge reading of gas in tank (in %), tank capacity (gal)
+
+var mpg = prompt("Please enter your car's MPG.");
+var gasReading = prompt("How much is currently in your gas tank (in %).");
+var tankCap = prompt("What is the capacity of your tank?");
+
+var miles = (tankCap * mpg)* gasReading;
+console.log(miles);
+
+if(miles > 200){
+    console.log("Yes, you can make it without stopping for gas!");
+    alert("Yes, you can make it without stopping for gas!")
+}else if(miles < 200){
+    console.log("You only have " + miles + " miles left in your gas tank, better get gas now while you can.");
+    alert("You only have " + miles + " miles left in your gas tank, better get gas now while you can.")
+}
+
+
