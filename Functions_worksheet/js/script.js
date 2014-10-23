6fa052a2661670cb7a64d6c9eb5aea7aa5e612b0
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-23 15:23:43 -0700

    Functions
    Worksheet

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -3,4 +3,20 @@
  */
 //Functions worksheet October 2014
 
+//Circumference
 
+function circumf(radius){
+    var circumf = radius * 2 * 3.14;
+    console.log("The circumference of the circle is " + circumf)
+}
+circumf(10);
+
+
+//Sting!
+
+function beeSting(weight){
+    var beeSting = weight / 8.666666667;
+    console.log("It takes " + beeSting + " bee stings to kill this animal.")
+}
+
+beeSting(140);
