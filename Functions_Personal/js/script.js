2aa4ea0a45f9a1739e244adfcb8474557cc8806b
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-25 23:07:44 -0700

    Function
    Personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -3,11 +3,12 @@
  */
 //Functions Personal Karri Wright October 2014
 
-//Home renovations
+//Home decorations
 
-var perCheck = 100
-var budget = 500
+var perCheck = 100;
+var budget = 500;
 var decor = prompt("How much is the item you want to buy?");
+    console.log(decor);
 
 if(decor < perCheck && budget > 500){
     console.log("We have enough to buy the decor.");
