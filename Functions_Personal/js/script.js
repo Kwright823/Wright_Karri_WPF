9d37b307403c98391d13c2184230ab29a559b8af
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-25 23:03:00 -0700

    Functions
    Personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,3 +2,16 @@
  * Created by karriwright on 10/23/14.
  */
 //Functions Personal Karri Wright October 2014
+
+//Home renovations
+
+var perCheck = 100
+var decor = 400
+var budget = 500
+
+if(decor < perCheck && budget > 500){
+    console.log("We have enough to buy the decor.");
+}else{
+    console.log("Keep saving!");
+}
+
