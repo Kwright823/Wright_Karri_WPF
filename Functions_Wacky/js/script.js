cd91bdf4618e6ef207a6de4ab6af3392d09f3e2a
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-25 23:35:38 -0700

    function
    wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,14 +6,16 @@
 
 //What should I make my kids for lunch?
 
-var kid1 = ("Is this for Ava or JJ?");
-var bread = ("Do you have bread?");
+var kid1 = prompt("Is this for Ava or JJ?");
 
 if(kid1 === "Ava"){
     console.log("Make Ava a PB&J or Nutella Sandwich.")
 }else{
     console.log("Make JJ a bologna or PB&J sandwich")
 }
+
+var bread = prompt("Do you have bread?");
+
 if(bread === "yes"){
     console.log("Make a sandwich.")
 }else{
