34f36374dc7117cdf16ea4ed80d304c7263a1f18
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-25 23:13:31 -0700

    Functions
    industry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,3 +2,10 @@
  * Created by karriwright on 10/23/14.
  */
 //Functions Industry Karri Wright October 2014
+
+//find the circumference
+var radius = prompt("What is the radius?");
+var diamet = radius * 2;
+var circumf = diamet * 3.14;
+console.log(circumf);
+alert("The circumference is " + circumf);
