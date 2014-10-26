cae7cd08f29a1d2c89f6bde57838187e4c68da13
Kwright823 <karriwright@Karris-MacBook-Pro.local>
2014-10-25 23:33:18 -0700

    functions
    industry

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,3 +2,15 @@
  * Created by karriwright on 10/23/14.
  */
 //Functions Wacky Karri Wright October 2014
+
+
+//What should I make my kids for lunch?
+
+var kid1 = ("Is this for Ava or JJ?");
+var bread = ("Do you have bread?");
+
+if(kid1 === "Ava"){
+    console.log("Make Ava a PB&J or Nutella Sandwich.")
+}else{
+    console.log("Make JJ a ")
+}
